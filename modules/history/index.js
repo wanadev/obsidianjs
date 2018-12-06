a6910e5e576b65a0008671f3d663379878457f3f
Timotée Neullas <tneullas@wanadev.fr>
2018-12-06 11:04:50 +0100

    Adding maxlength to module's default configuration

--- modules/history/index.js
+++ modules/history/index.js
@@ -3,6 +3,10 @@ module.exports = {
     name: "history",
     requires: ["data-store"],
 
+    config: {
+        maxLength: 50,
+    },
+
 
     load() {
         const History = require("./src/history"); // eslint-disable-line global-require
